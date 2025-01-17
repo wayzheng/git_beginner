c74e4d0a9d825e6a47c967664179d416bfa6bd2e
wenynag <3011223675@qq.com>
2025-01-17 19:35:50 +0800

    update files

---
+++ demo.py
@@ -0,0 +1,15 @@
+import random
+print("这是一个随机数%d"%random.randint(1, 100))
+for i in range(random.randint(1, 100)):
+    print(f"这是一个随机数字{random.randint(1, i+1)}")
+def demo(function):
+    def demo1():
+        function()
+        print("这是一个装饰器函数")
+    return demo1
+
+@demo
+def test():
+    print("这是一个测试函数")
+    
+test() # 最后的结果是：这是一个测试函数 这是一个装饰器函数 这是由于装饰器函数得到的结果
